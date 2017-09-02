35aa3c99a89582f257e202a50c0ae28a8bd5c4a2
Sherlin Antoine <sherlinthb@netscape.net>
2017-09-02 11:06:51 -0400

    adding scripts and rest of files

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void play()
+    {
+        SceneManager.LoadScene("PracticeScene");
+    }
+
+    public static void GoToTitleScreen()
+    {
+        SceneManager.LoadScene("TitleMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void titleScreen()
+    {
+        SceneManager.LoadScene("TitleMenu");
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
+
+    Player player;
+    
+
+    void Start()
+    {
+
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 directionalInput = new Vector2(CrossPlatformInputManager.GetAxisRaw("Horizontal"), CrossPlatformInputManager.GetAxisRaw("Vertical"));
+        Vector2 directionalInput2 = new Vector2(1, Input.GetAxisRaw("Vertical"));
+
+        //player.SetDirectionalInput(directionalInput);
+        player.SetDirectionalInput(directionalInput2);
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            player.OnjumpInputDown();
+        }
+
+        if (CrossPlatformInputManager.GetButtonUp("Jump"))
+        {
+            player.OnJumpInputUp();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.OnjumpInputDown();
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            player.OnJumpInputUp();
+        }
+    }
+
+    // not being used currently
+    public void upKey()
+    {
+
+    }
+
+    public void rightKey()
+    {
+        Input.GetButton("right");
+    }
+
+    public void leftKey()
+    {
+        Input.GetButton("left");
+    }
+
+    public void downKey()
+    {
+        player.OnjumpInputDown();
+    }
+
+    public void JumpKey()
+    {
+        player.OnJumpInputUp();
+    }
+}
